354634c94bc8979df6942ba47de5a44203eb6a6a
chiomavera <you@example.com>
2022-06-22 21:12:32 +0100

    collection made to division operator and two more operators added (modulus and exponential)

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
 //program for a simple calculator that will perform bsic arithmetics.
 function calculator() {
-    //take operand input
-    const num1 = parseFloat(window.prompt('Enter first number: '));
-    const num2 = parseFloat(window.prompt('Enter second number: '));
-    
-    //take operator input
-    const operator = window.prompt('Enter operator (either +, -, * or /): ');
+  //take operand input
+  const num1 = parseFloat(window.prompt("Enter first number: "));
+  const num2 = parseFloat(window.prompt("Enter second number: "));
 
-    let result;
-    // computation
+  //take operator input
+  const operator = window.prompt("Enter operator (either +, -, * or /): ");
 
-    switch (operator){
-        case '+':
-            result = num1 + num2;
-            document.write(`${num1} - ${num2} = ${result}`);
-            break;
+  let result;
+  // computation
 
-        case '-':
-            result = num1 - num2;
-            document.write(`${num1} - ${num2} = ${result}`);
-            break;
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      document.write(`${num1} - ${num2} = ${result}`);
+      break;
 
-        case '*':
-            result = num1 * num2;
-            document.write(`${num1} * ${num2} = ${result}`);
-            break;
+    case "-":
+      result = num1 - num2;
+      document.write(`${num1} - ${num2} = ${result}`);
+      break;
 
-        case '/':
-            result = num1 - num2;
-            document.write(`${num1} / ${num2} = ${result}`);
-            break;
-        
-        default:
-            document.write('invalid operator');
-            break;
-    }
+    case "*":
+      result = num1 * num2;
+      document.write(`${num1} * ${num2} = ${result}`);
+      break;
+
+    case "/":
+      result = num1 / num2;
+      document.write(`${num1} / ${num2} = ${result}`);
+      break;
+
+    case "%":
+      result = num1 % num2;
+      document.write(`${num1} % ${num2} = ${result}`);
+      break;
+
+    case "^":
+      result = num1 ^ num2;
+      document.write(`${num1} ^ ${num2} = ${result}`);
+      break;
+
+    default:
+      document.write("invalid operator");
+      break;
+  }
 }
 
 calculator();
